1c3816c0551320179574445419f1e642cba42d1c
shanu G <shanug@shanus-MacBook-Pro.local>
2023-07-05 01:31:48 +0530

    debug off

--- TV369_Backend/TV369_Backend/settings.py
+++ TV369_Backend/TV369_Backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&hqbtvmhl8l90v%=ip-ws*67i$u$ae#&n9$se6wh^!94+infl3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
