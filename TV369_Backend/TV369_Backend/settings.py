c28bae756041ee7ddfb7cb250cfb7ece0b7cb04e
saitejD <saitej.dande@gmail.com>
2023-07-05 23:29:00 +0530

    media url and cors error

--- TV369_Backend/TV369_Backend/settings.py
+++ TV369_Backend/TV369_Backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +34,7 @@ SWAGGER_SETTINGS = {
     'DEFAULT_INFO': 'myapp.urls.openapi_info',
 }
 
-CORS_ALLOW_ALL_ORIGINS =True
+CORS_ORIGIN_ALLOW_ALL = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -148,6 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
